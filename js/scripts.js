f15aa947552b0419f8bec2916e8c809eef73cbc7
Joseph Bertino <jbertin@sandia.gov>
2021-02-10 11:53:33 -0700

    meets expectations, but needs comments and code cleanup

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,11 @@
 // script.js
 // Author: Joe Bertino, 2021
 
-// JBJB DELETE THIS
-fetch('https://randomuser.me/api/?format=json&results=12')
-.then(res => res.json())
-.then(data => {
-    alert(data.results[0]);
-    console.log(data);
-});
+// Document Elements
+const gallery = document.querySelector('div.gallery');
+const searchContainer = document.querySelector('div.search-container');
 
 // Add Markup to the Employee Directory Page
-const searchContainer = document.querySelector('div.search-container');
 const searchHTML = `
     <form action="#" method="get">
         <input type="search" id="search-input" class="search-input" placeholder="Search...">
@@ -19,127 +14,79 @@ const searchHTML = `
     `;
 searchContainer.insertAdjacentHTML('beforeend', searchHTML);
 
-const gallery = document.querySelector('div.gallery');
-// JBJB This will probably be in a function
-/**
-<div class="card">
-    <div class="card-img-container">
-        <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-    </div>
-    <div class="card-info-container">
-        <h3 id="name" class="card-name cap">first last</h3>
-        <p class="card-text">email</p>
-        <p class="card-text cap">city, state</p>
-    </div>
-</div>
-*/
+// Fetch the Random User data with the API
+const infoList = "name,location,email,picture,cell,dob";
+let userProfiles;
+fetch(`https://randomuser.me/api/?format=json&results=12&inc=${infoList}&nat=us`)
+.then(res => res.json())
+.then(data => {
+    userProfiles = data.results;
+    userProfiles.map(drawUserCard);
+});
 
-// JBJB Modal markup
 /**
-    Supposed to 'append this to the body' but maybe put it after the div.gallery?
-<div class="modal-container">
-    <div class="modal">
-        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-        <div class="modal-info-container">
-            <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-            <h3 id="name" class="modal-name cap">name</h3>
-            <p class="modal-text">email</p>
-            <p class="modal-text cap">city</p>
-            <hr>
-            <p class="modal-text">(555) 555-5555</p>
-            <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-            <p class="modal-text">Birthday: 10/21/2015</p>
+ *
+ */
+function drawUserCard(user, idx) {
+    const name = user.name;
+    const location = user.location;
+    const div = document.createElement('div');
+    div.className = "card";
+    div.id = `user_${idx}`;
+    console.log(user, idx);
+    const cardHTML = `
+        <div class="card-img-container">
+            <img class="card-img" src="${user.picture.large}" alt="profile picture">
         </div>
-    </div>
-
-    IMPORTANT: Below is only for exceeds tasks 
-    <div class="modal-btn-container">
-        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-        <button type="button" id="modal-next" class="modal-next btn">Next</button>
-    </div>
-</div>
-*/
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${name.first} ${name.last}</h3>
+            <p class="card-text">${user.email}</p>
+            <p class="card-text cap">${location.city}, ${location.state}</p>
+        </div>
+        `;
+    div.insertAdjacentHTML('beforeend', cardHTML);
+    div.addEventListener('click', drawUserModal);
+    gallery.appendChild(div);
+}
 
 /**
-
-https://randomuser.me/api/?inc=gender,name,nat
-    gender
-    name
-    location
-    email
-    login
-    registered
-    dob
-    phone
-    cell
-    id
-    picture
-    nat
-
-If our API service is offline or if we are experiencing server issues, we'll return a simple JSON object with an error.
-
-{
-  "results": [
-    {
-      "gender": "male",
-      "name": {
-        "title": "mr",
-        "first": "brad",
-        "last": "gibson"
-      },
-      "location": {
-        "street": "9278 new road",
-        "city": "kilcoole",
-        "state": "waterford",
-        "postcode": "93027",
-        "coordinates": {
-          "latitude": "20.9267",
-          "longitude": "-7.9310"
-        },
-        "timezone": {
-          "offset": "-3:30",
-          "description": "Newfoundland"
-        }
-      },
-      "email": "brad.gibson@example.com",
-      "login": {
-        "uuid": "155e77ee-ba6d-486f-95ce-0e0c0fb4b919",
-        "username": "silverswan131",
-        "password": "firewall",
-        "salt": "TQA1Gz7x",
-        "md5": "dc523cb313b63dfe5be2140b0c05b3bc",
-        "sha1": "7a4aa07d1bedcc6bcf4b7f8856643492c191540d",
-        "sha256": "74364e96174afa7d17ee52dd2c9c7a4651fe1254f471a78bda0190135dcd3480"
-      },
-      "dob": {
-        "date": "1993-07-20T09:44:18.674Z",
-        "age": 26
-      },
-      "registered": {
-        "date": "2002-05-21T10:59:49.966Z",
-        "age": 17
-      },
-      "phone": "011-962-7516",
-      "cell": "081-454-0666",
-      "id": {
-        "name": "PPS",
-        "value": "0390511T"
-      },
-      "picture": {
-        "large": "https://randomuser.me/api/portraits/men/75.jpg",
-        "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
-        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
-      },
-      "nat": "IE"
-    }
-  ],
-  "info": {
-    "seed": "fea8be3e64777240",
-    "results": 1,
-    "page": 1,
-    "version": "1.3"
-  }
+ *
+ */
+function getDOB(dobstr) {
+    const yyyymmdd = dobstr.match(/^(\d{4})-(\d{2})-(\d{2})/);
+    return `${yyyymmdd[2]}/${yyyymmdd[3]}/${yyyymmdd[1]}`;
 }
-*/
 
-//JBJB You are encouraged to experiment with things like color, background color, font, shadows, transitions and animations to make this project your own.
+/**
+ *
+ */
+function drawUserModal(event) {
+    const userId = event.currentTarget.id.match(/^user_(\d*)$/)[1]
+    const user = userProfiles[userId];
+    const location = user.location;
+    const bday = getDOB(user.dob.date);
+    const fullAddr = `${location.street.number} ${location.street.name}, ${location.city}, ${location.state} ${location.postcode}`;
+    
+    const modalHTML = `
+        <div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="${user.picture.large}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+                    <p class="modal-text">${user.email}</p>
+                    <p class="modal-text cap">${location.city}</p>
+                    <hr>
+                    <p class="modal-text">${user.cell}</p>
+                    <p class="modal-text">${fullAddr}</p>
+                    <p class="modal-text">Birthday: ${bday}</p>
+                </div>
+            </div>
+        </div>
+    `;
+    gallery.insertAdjacentHTML('afterend',modalHTML);
+    
+    document.getElementById('modal-close-btn').addEventListener('click', (e) => {
+        document.querySelector('div.modal-container').remove();
+    });
+}
